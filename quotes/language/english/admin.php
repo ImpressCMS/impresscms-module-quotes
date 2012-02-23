72e6acc0bae6ee4098322dbd2c599bc68f535502
Simon <simon@isengard.biz>
2012-02-23 01:18:18 +0000

    Filing 1.0 final release.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/quotes/releases/1.0%20final@23684 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ quotes/language/english/admin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * English language constants used in admin section of the module
+ *
+ * @copyright	Copyright Madfish (Simon Wilkinson) 2011
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+ * @package		quotes
+ * @version		$Id$
+ */
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+// Requirements
+define("_AM_QUOTES_REQUIREMENTS", "Quotes Requirements");
+define("_AM_QUOTES_REQUIREMENTS_INFO", "We've reviewed your system, unfortunately it doesn't meet 
+	all the requirements needed for Quotes to function. Below are the requirements needed.");
+define("_AM_QUOTES_REQUIREMENTS_ICMS_BUILD", "Quotes requires at least ImpressCMS 1.3.");
+define("_AM_QUOTES_REQUIREMENTS_SUPPORT", "Should you have any question or concerns, please visit 
+	our forums at <a href='http://community.impresscms.org'>http://community.impresscms.org</a>.");
+
+// Quote
+define("_AM_QUOTES_QUOTES", "Quotes");
+define("_AM_QUOTES_QUOTES_DSC", "All quotes in the module");
+define("_AM_QUOTES_QUOTE_CREATE", "Add a quote");
+define("_AM_QUOTES_QUOTE", "Quote");
+define("_AM_QUOTES_QUOTE_CREATE_INFO", "Fill-out the following form to create a new quote.");
+define("_AM_QUOTES_QUOTE_EDIT", "Edit this quote");
+define("_AM_QUOTES_QUOTE_EDIT_INFO", "Fill-out the following form in order to edit this quote.");
+define("_AM_QUOTES_QUOTE_MODIFIED", "The quote was successfully modified.");
+define("_AM_QUOTES_QUOTE_CREATED", "The quote has been successfully created.");
+define("_AM_QUOTES_QUOTE_VIEW", "Quote info");
+define("_AM_QUOTES_QUOTE_VIEW_DSC", "Here is the info about this quote.");
+define("_AM_QUOTES_QUOTE_ONLINE", "Quote toggled online.");
+define("_AM_QUOTES_QUOTE_OFFLINE", "Quote toggled offline.");
---
+++ quotes/language/english/common.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * English language constants commonly used in the module
+ *
+ * @copyright	Copyright Madfish (Simon Wilkinson) 2011
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+ * @package		quotes
+ * @version		$Id$
+ */
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+// quote
+define("_CO_QUOTES_QUOTE_CREATOR", "Author");
+define("_CO_QUOTES_QUOTE_CREATOR_DSC", "The person to which you attribute this quote.");
+define("_CO_QUOTES_QUOTE_DESCRIPTION", "Quote");
+define("_CO_QUOTES_QUOTE_DESCRIPTION_DSC", "Enter your quotation here.");
+define("_CO_QUOTES_QUOTE_DATE", "Date");
+define("_CO_QUOTES_QUOTE_DATE_DSC", "The date the quote was made (usually, a year).");
+define("_CO_QUOTES_QUOTE_ONLINE_STATUS", "Online?");
+define("_CO_QUOTES_QUOTE_ONLINE_STATUS_DSC", "Toggle this quote online or offline.");
+define("_CO_QUOTES_QUOTE_ONLINE", "Online");
+define("_CO_QUOTES_QUOTE_OFFLINE", "Offline");
